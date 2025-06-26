ee63d2fc03f3255ae4353e2f8458863816259879
m4tice <ductuan2908@gmail.com>
2025-06-27 00:49:47 +0700

    feat: add console log

--- miniapp/static/app_start.js
+++ miniapp/static/app_start.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.btn');
     buttons.forEach(button => {
         button.addEventListener('click', function() {
+            console.log('Button clicked:', this.textContent);
             alert('You have selected ' + this.textContent);
             window.location.href = 'app_session.html';
         });
